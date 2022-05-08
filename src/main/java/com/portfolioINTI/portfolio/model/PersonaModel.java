b7b6201176b81fa2bb6a9f71bf159759bc986998
matiasgasparg <matiasgasparg@gmail.com>
2022-05-07 22:50:34 -0300

    Creacion del model persona

--- src/main/java/com/portfolioINTI/portfolio/model/PersonaModel.java
+++ src/main/java/com/portfolioINTI/portfolio/model/PersonaModel.java
@@ -4,10 +4,13 @@
  */
 package com.portfolioINTI.portfolio.model;
 
+import java.util.logging.Logger;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +18,13 @@ import lombok.Setter;
  *
  * @author walke
  */
-@Getter @Setter
+
 @Entity
+@Table (name="persona")
 public class PersonaModel {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)  
+    //@GeneratedValue(strategy=GenerationType.IDENTITY)  
+    @Column (unique= true ,nullable= false)
         private int idpersona;
         
         private String nombre;
@@ -48,6 +53,99 @@ public class PersonaModel {
         this.fotourl = fotourl;
         this.username = username;
         this.password = password;
+    }
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getAcerca() {
+        return acerca;
+    }
+
+    public void setAcerca(String acerca) {
+        this.acerca = acerca;
+    }
+
+    public String getFotourl() {
+        return fotourl;
+    }
+
+    public void setFotourl(String fotourl) {
+        this.fotourl = fotourl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonaModel{" + "idpersona=" + idpersona + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo + ", titulo=" + titulo + ", telefono=" + telefono + ", ubicacion=" + ubicacion + ", acerca=" + acerca + ", fotourl=" + fotourl + ", username=" + username + ", password=" + password + '}';
     }
         
                 
