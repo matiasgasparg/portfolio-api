16e4b8c2b56a1dd337142fa151ae866ef0765b83
matiasgasparg <matiasgasparg@gmail.com>
2022-05-08 04:31:47 -0300

    make it better

--- src/main/java/com/portfolioINTI/portfolio/controller/ProyectoController.java
+++ src/main/java/com/portfolioINTI/portfolio/controller/ProyectoController.java
@@ -61,6 +61,6 @@ public class ProyectoController {
    
    public String deleteProyecto(@PathVariable int id){
        proyectoService.deleteProyecto(id);
-       return "El proyecto fue eliminado correctamente";
+       return "El proyecto fue eliminadx correctamente";
    }
 }
