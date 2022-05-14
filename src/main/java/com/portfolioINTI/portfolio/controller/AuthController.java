e589963218dede2f6497c73737b08189752ab7b1
matiasgasparg <matiasgasparg@gmail.com>
2022-05-13 21:15:44 -0300

    c

--- src/main/java/com/portfolioINTI/portfolio/controller/AuthController.java
+++ src/main/java/com/portfolioINTI/portfolio/controller/AuthController.java
@@ -44,7 +44,7 @@ public class AuthController {
 								.map(GrantedAuthority::getAuthority)
 								.collect(Collectors.toList()))
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.setExpiration(new Date(System.currentTimeMillis() + 60*60*1000))
 				.signWith(SignatureAlgorithm.HS512,
 						secretKey.getBytes()).compact();
 
