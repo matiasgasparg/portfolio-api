421e951ea26dcda711bfdb62719736ad09e3b556
matiasgasparg <matiasgasparg@gmail.com>
2022-05-24 23:13:12 -0300

    make it better

--- src/main/java/com/portfolioINTI/portfolio/controller/RedesController.java
+++ src/main/java/com/portfolioINTI/portfolio/controller/RedesController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author walke
  */
+@CrossOrigin("*")
 
 @RestController
 @RequestMapping("/api/redes")
