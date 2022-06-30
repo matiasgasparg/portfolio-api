907954241d7a6f6c5d49ec389bc125c35c2f294f
matiasgasparg <matiasgasparg@gmail.com>
2022-06-30 17:42:29 -0300

    a

--- src/main/java/com/portfolioINTI/portfolio/model/LaboralModel.java
+++ src/main/java/com/portfolioINTI/portfolio/model/LaboralModel.java
@@ -5,7 +5,6 @@
 package com.portfolioINTI.portfolio.model;
 
 import java.time.LocalDate;
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
